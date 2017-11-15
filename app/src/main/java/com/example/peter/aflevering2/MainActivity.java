e05172ce5e431e57047a7e8a4ec7a9367c41e812
rosenberg71 <peterrosenberg71@gmail.com>
2017-11-15 20:16:57 +0100

    Initial project

---
+++ app/src/main/java/com/example/peter/aflevering2/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.peter.aflevering2;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    public static final String TAG = "Aflevering 2";
+    private static final double STORCENTER_NORD_LATITUDE = 56.170608;
+    private static final double STORCENTER_NORD_LONGITUDE = 10.188461;
+    private static final float STORCENTER_NORD_RADIUS = 1500;
+    private GoogleApiClient mGoogleApiClient;
+    boolean mRequestingLocationUpdates; //has user turned location updates on or off?
+    private String mLastUpdateTime;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    private Geofence mStorcenterNordFence;
+    private GeofencingRequest mGeofencingRequest;
+    private PendingIntent mPendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Trying to create geofence", Toast.LENGTH_LONG).show();
+
+        //Setup Google API
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mLocationRequest = new LocationRequest();
+        mRequestingLocationUpdates = true; //Starting the app the location updating is on
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        //Create fence around Storcenter Nord
+        mStorcenterNordFence = new Geofence.Builder().setRequestId("storcenter_nord")
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                .setCircularRegion(STORCENTER_NORD_LATITUDE, STORCENTER_NORD_LONGITUDE, STORCENTER_NORD_RADIUS)
+                .build();
+        //Create Geofencing Request
+        mGeofencingRequest = new GeofencingRequest.Builder()
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                .build();
+
+        //Create Intent pointing to intent service
+        Intent intent = new Intent(this, ReceiveGeoFenceTransitionService.class);
+        mPendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //some permission stuff created by android Studio
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.GeofencingApi.addGeofences(mGoogleApiClient, mGeofencingRequest, mPendingIntent);
+        Toast.makeText(getApplicationContext(), "Geofence added!?", Toast.LENGTH_LONG).show();
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+    }
+
+    private void updateUI(){
+    }
+}
---
+++ app/src/main/java/com/example/peter/aflevering2/ReceiveGeoFenceTransitionService.java
@@ -0,0 +1,106 @@
+package com.example.peter.aflevering2;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+/**
+ * Created by Peter on 15-11-2017.
+ */
+
+public class ReceiveGeoFenceTransitionService extends IntentService {
+
+    // Notification channel ID, needed for API 26 and higher
+    public final static String CHANNEL_ID = "default";
+
+    public ReceiveGeoFenceTransitionService() {
+        super("ReceiveGeoFenceTransitionService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+
+        if (event.hasError()) {
+            // TODO: Handle error
+        } else {
+            int transition = event.getGeofenceTransition();
+
+            if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                String transitionType = getTransitionString(transition);
+
+
+                // Send a notification, when clicked, open website
+                String url = "https://www.rejseplanen.dk/webapp/index.html?language=en_EN&#!S|Aarhus%20H!Z|%C3%85bogade%2034%2C%208200%20Aarhus%20N%2C%20Aarhus%20Kommune!start|1";
+                Intent notificationIntent = new Intent(Intent.ACTION_VIEW);
+                notificationIntent.setData(Uri.parse(url));
+
+                PendingIntent contentIntent = PendingIntent.getActivity(this.getApplicationContext(), 0, notificationIntent, 0);
+
+                // Create a notification channel if on API 26 (Android O) and above
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Default Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+                    // Configure the notification channel.
+                    notificationChannel.setDescription("Channel description");
+                    notificationChannel.enableLights(true);
+                    notificationChannel.setLightColor(Color.RED);
+                    notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+                    notificationChannel.enableVibration(true);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+
+                Notification notification = new NotificationCompat.Builder(this.getApplicationContext(), CHANNEL_ID)
+                        .setContentTitle("Near Aarhus H!")
+                        .setContentText("Check out how to get to the Datalogi building!")
+                        .setTicker("You're near the Hovedbanegaard. Do you want to see how to get to the CS building?")
+                        .setContentIntent(contentIntent)
+                        .setAutoCancel(true)
+                        .build();
+
+                Log.d(MainActivity.TAG, "Notification created");
+
+                NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
+                manager.notify(1, notification);
+
+                Log.d(MainActivity.TAG, "Notified!");
+            } else {
+                // TODO: Handle invalid transition
+            }
+        }
+    }
+
+    /**
+     * Maps geofence transition types to their human-readable equivalents.
+     *
+     * @param transitionType A transition type constant defined in Geofence
+     * @return A String indicating the type of transition
+     */
+    private String getTransitionString(int transitionType) {
+        switch (transitionType) {
+
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return "Entered";
+
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return "Exited";
+
+            default:
+                return "Unknown transition";
+        }
+    }
+}
